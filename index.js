3d3acc4b33d9ea7896c36447134f190da690c611
sundeepnarang <sundeepnrng@gmail.com>
2022-01-26 01:07:51 -0500

    improved fetch

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const {Octokit} = require("@octokit/core");
 const {personalAccessToken} = require("./config");
+const fs = require("fs");
 
 const octokit = new Octokit({ auth: personalAccessToken });
 
-async function main() {
+async function fetch(page="1") {
     const {data} = await octokit.request('GET /orgs/{org}/repos', {
         org: 'qualityedgar',
         per_page: '100',
-        page: '3'
+        page: page
     })
-    data.map(async ({id,name, full_name, owner:{login}={}}={})=>{
-        const {data: contributors=[]} = await octokit.request('GET /repos/{owner}/{repo}/contributors', {
+    const unresolvedPromises = data.map(async ({id,name, full_name, owner:{login}={}}={})=>{
+        const {data: collaborators=[]} = await octokit.request('GET /repos/{owner}/{repo}/collaborators', {
             owner: login,
             repo: name
         });
@@ -18,16 +19,23 @@ async function main() {
             owner: login,
             repo: name
         });
-        console.log(JSON.stringify(
-            {
-                id,
-                full_name,
-                contributors: contributors.map(d=>d.login),
-                teams: teams.map(d=>d.name)
-            },
-            null,2
-        ));
+        return {
+            id,
+            full_name,
+            collaborators: collaborators.map(d=>{return { name: d.login, admin: d.permissions.admin }}),
+            teams: teams.map(d=>d.name)
+        }
     })
+
+    const results = await Promise.all(unresolvedPromises);
+    return results;
 }
 
-main();
+async  function main(){
+    const pg1 = await fetch();
+    const pg2 = await fetch("2");
+    fs.writeFileSync("output.js", `module.exports = ${JSON.stringify(pg1.concat(pg2),null, 2)}`)
+}
+
+
+main().then(r => console.log("done"));
